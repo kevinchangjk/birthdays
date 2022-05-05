193aacfaee086c6f5fbef84faf19412ff3db8c69
kevinchangjk <kevinchangjk@gmail.com>
2022-05-05 17:41:57 +0800

    updated gitignore again

--- api-test.js
+++
@@ -1,8 +0,0 @@
-import {
-  createDatabase,
-  readDatabase,
-  rebuildDatabase,
-  writeDatabase,
-} from "../src/data-api.js";
-
-import { enumerateData, queryDate, queryName } from "../src/user-api.js";
--- node_modules/csv-generate/dist/esm/sync.d.ts
+++
@@ -1,6 +0,0 @@
-
-import { Options } from './index';
-
-declare function generate<T = any>(options: number | Options): string & Array<T>;
-// export default generate;
-export { generate, Options };
