469286874830faa2c63e901d9bf88185dfec56f1
kevinchangjk <kevinchangjk@gmail.com>
2022-05-06 12:24:08 +0800

    reorganised flow

--- src/commands/query-date.js
+++ src/commands/query-date.js
@@ -1,10 +1,37 @@
-import { queryDate, checkToday } from "../user-api.js";
+import { queryDate } from "../user-api.js";
+
+function formatDate(num) {
+  const actual = num + 1;
+  if (actual < 10) {
+    return "0" + actual.toString();
+  } else {
+    return actual.toString();
+  }
+}
+
+function getDate() {
+  const today = new Date();
+  const day = formatDate(today.getDay());
+  const month = formatDate(today.getMonth());
+  return day + month;
+}
 
 const args = process.argv.slice(2);
 if (args.length == 0) {
-  checkToday();
+  const date = getDate();
+  const bucket = queryDate("1205");
+  if (bucket.length > 0) {
+    console.log(`\n🎉🎉🎉 Today, on ${bucket[0].niceDate()} 🎉🎉🎉\n`);
+    bucket.forEach((birthday) => birthday.celebrate());
+  }
 } else {
   args.forEach(function (arg) {
-    queryDate(arg);
+    const bucket = queryDate(arg);
+    if (bucket.length === 0) {
+      console.log(`No birthdays found on ${temp.niceDate()}`);
+    } else {
+      console.log(`On ${bucket[0].niceDate()}: `);
+      bucket.forEach((birthday) => birthday.display());
+    }
   });
 }
--- src/user-api.js
+++ src/user-api.js
@@ -5,48 +5,17 @@ import {
   writeDatabase,
 } from "./data-api.js";
 
-function formatDate(num) {
-  const actual = num + 1;
-  if (actual < 10) {
-    return "0" + actual.toString();
-  } else {
-    return actual.toString();
-  }
-}
-
-function getDate() {
-  const today = new Date();
-  const day = formatDate(today.getDay());
-  const month = formatDate(today.getMonth());
-  return day + month;
-}
-
 const database = await readDatabase();
 
-export async function enumerateData() {
+export function enumerateData() {
   database.listAll();
 }
 
-export async function queryDate(date) {
-  const bucket = database.queryDate(date);
-  if (bucket.length == 0) {
-    console.log(`No birthdays found on ${temp.niceDate()}`);
-  } else {
-    console.log(`On ${bucket[0].niceDate()}: `);
-    bucket.forEach((birthday) => birthday.display());
-  }
+export function queryDate(date) {
+  return database.queryDate(date);
 }
 
-export async function checkToday() {
-  const date = getDate();
-  const bucket = database.queryDate(date);
-  if (bucket.length > 0) {
-    console.log(`\n🎉🎉🎉 Today, on ${bucket[0].niceDate()} 🎉🎉🎉\n`);
-    bucket.forEach((birthday) => birthday.celebrate());
-  }
-}
-
-export async function queryName(name) {
+export function queryName(name) {
   database.queryName(name);
 }
 
