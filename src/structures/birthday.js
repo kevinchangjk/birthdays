7b2d6b11a34ee914459c77c478db72f5994b45bd
kevinchangjk <kevinchangjk@gmail.com>
2022-05-07 00:46:11 +0800

    made total days function in birthday.js cleaner

--- src/structures/birthday.js
+++ src/structures/birthday.js
@@ -7,28 +7,20 @@ function dateToIndex(date) {
     switch (month) {
       case 1:
         return 0;
-      case 2:
-        return 31 + totalDays(month - 1);
       case 3:
         return 29 + totalDays(month - 1);
-      case 4:
-        return 31 + totalDays(month - 1);
       case 5:
-        return 30 + totalDays(month - 1);
-      case 6:
-        return 31 + totalDays(month - 1);
       case 7:
-        return 30 + totalDays(month - 1);
-      case 8:
-        return 31 + totalDays(month - 1);
-      case 9:
-        return 31 + totalDays(month - 1);
       case 10:
-        return 30 + totalDays(month - 1);
-      case 11:
-        return 31 + totalDays(month - 1);
       case 12:
         return 30 + totalDays(month - 1);
+      case 2:
+      case 4:
+      case 6:
+      case 8:
+      case 9:
+      case 11:
+        return 31 + totalDays(month - 1);
       default:
         return 0;
     }
