1dc82669078d63e3c9617682abdf8cd03fb89e97
kevinchangjk <kevinchangjk@gmail.com>
2022-05-07 12:11:19 +0800

    added setup script

--- src/commands/query-name.js
+++ src/commands/query-name.js
@@ -1,12 +1,16 @@
 import { queryName } from "../user-api.js";
 
 const args = process.argv.slice(2);
+
+// TODO: ask user to input at least one argument
+
 for (const arg of args) {
   const results = await queryName(arg);
   const count = results.length;
   const dispRes = count == 1 ? "result" : "results";
+  console.log("\n----------------------------------------");
   for (const result of results) {
     result.display();
   }
-  console.log(`\n${count} ${dispRes} found`);
+  console.log(`\n${count} ${dispRes} found for "${arg}"`);
 }
